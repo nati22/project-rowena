c6fd1a14aa8a2b04a10bb18dbd53777e9320e391
Nati Tessema <nati.tessema@gmail.com>
2019-12-24 17:15:27 -0800

    Testing the config vars.

--- quiz/views.py
+++ quiz/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+import os
+
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello world. You're at the quiz page")
+    return HttpResponse(f"Hello world. You're at the quiz page (db user: {os.environ.get('DATABASE_USER')})")
