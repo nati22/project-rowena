73576154dcd76d4c9c84dec0a28b01306977306c
Nati Tessema <nati.tessema@gmail.com>
2019-12-26 18:46:39 -0800

    Add models.

--- quiz/models.py
+++ quiz/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class Question(models.Model):
+    text = models.CharField(max_length=400)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True, null=True)
+
+    def __str__(self):
+        return self.text
+
+
+class Answer(models.Model):
+    text = models.CharField(max_length=300)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True, null=True)
+    is_correct = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.text
