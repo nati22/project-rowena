78fecfe032920b64dc5dd831789d5233d375e6f8
Nati Tessema <nati.tessema@gmail.com>
2020-03-28 23:44:39 -0700

    Trying another solution to fix staticfiles issue.

--- rowena/settings.py
+++ rowena/settings.py
@@ -126,7 +126,7 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 
-STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
+STATICFILES_STORAGE = 'rowena.storage.WhiteNoiseStaticFilesStorage'
 
 DEBUG_PROPAGATE_EXCEPTIONS = True
 
