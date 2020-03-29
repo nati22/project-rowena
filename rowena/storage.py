85b8386744f7b43c55527ce36c4bde72fd10d0ff
Nati Tessema <nati.tessema@gmail.com>
2020-03-28 23:55:20 -0700

    Trying the whitenoise documentation approach again (#1)

--- rowena/storage.py
+++
@@ -1,6 +0,0 @@
-from whitenoise.storage import CompressedManifestStaticFilesStorage
-
-
-# Ripped off from https://stackoverflow.com/a/51580328/1446945
-class WhiteNoiseStaticFilesStorage(CompressedManifestStaticFilesStorage):
-    manifest_strict = False
